1c04583148424b5c5ff055431133631bc2fd2005
Matthew <106117475+MLindbloom@users.noreply.github.com>
2024-01-17 23:01:17 -0800

    index.js express, morgan, movies
    Added express and express routing syntax.
    Created top 10 movie list.
    Added morgan middleware and error-handling middleware

---
+++ index.js
@@ -0,0 +1,85 @@
+const express = require('express'),
+  morgan = require('morgan'),
+  fs = require('fs'),
+  path = require('path');
+
+const app = express();
+
+let topMovies = [
+  {
+    title: 'Interstellar',
+    director: 'Christopher Nolan',
+    year: '2014',
+  },
+  {
+    title: 'The Holiday',
+    director: 'Nancy Meyers',
+    year: '2006',
+  },
+  {
+    title: 'Avenger: Endgame',
+    director: 'Anthony Russo, Joe Russo',
+    year: '2019',
+  },
+  {
+    title: 'Top Gun: Maverick',
+    director: 'Joseph Kosinski',
+    year: '2022',
+  },
+  {
+    title: 'The Hangover',
+    director: 'Todd Phillips',
+    year: '2009',
+  },
+  {
+    title: 'Inception',
+    director: 'Christopher Nolan',
+    year: '2010',
+  },
+  {
+    title: 'The Lord of the Rings: Return of the King',
+    director: 'Peter Jackson',
+    year: '2001',
+  },
+  {
+    title: 'Rogue One',
+    director: 'Gareth Edwards',
+    year: '2016',
+  },
+  {
+    title: 'Harry Potter and the Deathly Hallows: Part 2',
+    director: 'David Yates',
+    year: '2011',
+  },
+  {
+    title: 'Pirates of the Caribbean: At World’s End',
+    director: 'Gore Verbinski',
+    year: '2007',
+  },
+];
+
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {
+  flags: 'a',
+});
+app.use(morgan('combined', { stream: accessLogStream }));
+app.use(express.static('public'));
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+app.get('/', function (req, res, next) {
+  res.send('Welcome to my app!');
+});
+
+app.get('/movies', (req, res) => {
+  res.json(topMovies);
+});
+
+app.get('/documentation.html', (req, res) => {
+  res.sendFile('public/documentation.html', { root: __dirname });
+});
+
+app.listen(8080, () => {
+  console.log('Your app is listening to port 8080.');
+});
