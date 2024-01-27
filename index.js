8083a7c8552958623f77c3e06283df672194ea95
Matthew <106117475+MLindbloom@users.noreply.github.com>
2024-01-26 22:06:15 -0800

    Index.js fixed typo
    Fixed typo, (user/users)

--- index.js
+++ index.js
@@ -222,14 +222,16 @@ app.post('/users/:id/:movieTitle', (req, res) => {
 //DELETE FAVORITE MOVIE
 app.delete('/users/:id/:movieTitle', (req, res) => {
   const { id, movieTitle } = req.params;
-  let user = user.find((user) => user.id == id);
+
+  let user = users.find((user) => user.id == id);
+
   if (user) {
     user.favoriteMovies = user.favoriteMovies.filter(
       (title) => title !== movieTitle,
     );
     res
       .status(200)
-      .send(`${movieTitle} has been removed from user ${id}'s array!`);
+      .send(`${movieTitle} has been removed from user ${id}'s array`);
   } else {
     res.status(400).send('No user found.');
   }
